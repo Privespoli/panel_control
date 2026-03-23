03aa096bbe419827063186c5be7e1880357836db
Priscila Nunes <priscilavespoli92@gmail.com>
2026-03-23 16:08:57 +0100

    Part A ok

---
+++ assets/java/alumnoA.js
@@ -0,0 +1,17 @@
+const miNombre = "Pri_&_Meli"; 
+const destinos = ["Neptuno", "Urano", "Marte"];
+
+
+const botonIdentificar = document.getElementById("btn-identificar");
+const pantallaNombre = document.getElementById("nombre-id");
+const pantallaDestino = document.getElementById("destino-id");
+
+
+botonIdentificar.addEventListener("click", function() {
+
+    pantallaNombre.innerText = miNombre;
+    pantallaDestino.innerText = destinos[1];
+    
+    
+    console.log("Comandante identificado! El destino es:" + destinos[1]);
+});
